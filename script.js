091811ecd794340c4353065fe301ac157448fa34
Bruna <brunasthephany.moraes@gmail.com>
2022-01-10 15:15:36 -0300

    Corrigido nome das imagens e adicionado ícone

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const image = document.getElementById('easter-egg-image');
 function onClick() {
   if (image.src.includes('png')) {
     audio.play();
-    image.src = 'images/gif-disco.gif';
+    image.src = 'images/disc-gif.gif';
   } else {
     audio.pause();
-    image.src = 'images/image-disco.png';
+    image.src = 'images/disc-image.png';
   }
 }
